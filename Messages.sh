d3ecd2cb13ac4389393969646f8b022d54ddb0ba
Yuri Chornoivan <yurchor@ukr.net>
2013-10-20 16:34:45 +0300

    Extract from ui files

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name \*.rc`  >> rc.cpp
+$EXTRACTRC `find . -name \*.rc -o -name \*.ui`  >> rc.cpp
 $XGETTEXT rc.cpp */*.cpp */*.h -o $podir/kcm_ufw.pot
 rm -f rc.cpp
